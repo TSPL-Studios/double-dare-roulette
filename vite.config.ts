204bca8e09abc5c4576c039f850e1ed42e63de55
Teamslepperlin0 <97560968+Teamslepperlino@users.noreply.github.com>
2025-08-11 04:46:53 +0200

    Aktualisieren von vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/double-dare-roulette/",    // <-- Hier der wichtige Pfad
   server: {
     host: "::",
     port: 8080,
